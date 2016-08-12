0eb2940d7bcb438990d465b7f9370cfc4ad19bf9
Amaan & Martin <pair+amaanvaddadi.martybutts@devbootcamp.com>
2016-08-12 14:31:40 +1200

    function working, sort of

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var Olympian = require('./Olympian')
 // On input value 0-3 by clicking the submit it says you are not qualified keep training
 // On input value 4 + by clicking the submit it says you are  qualified
 
-// listen for a click on the Submit button
-// when that happens, get the value from the input
+// listen for a click on the Submit button Y
+// when that happens, get the value from the input Y
 // use the value with the Olympian prototype
   //  instantiate (make a new) Olympian with the value from the form input
   //  use checkIfQualified method to check if the new olympian is ready
@@ -14,14 +14,18 @@ var Olympian = require('./Olympian')
       // if false
         // add KEEP TRAINING to page
 
-var input = document.getElementById('guest_name')
+var input = document.getElementById('yearsInput')
 
 var submit = document.getElementById('clicker');
 
 submit.addEventListener('click', function () {
 
+  var olympian = new Olympian(input.value)
+  console.log("Hey new Olympian", olympian)
   document.getElementById('display').innerHTML = input.value;
+
 })
+
 
 
 console.log("welcome to the Olympics")
